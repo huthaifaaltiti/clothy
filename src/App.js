66cb953f5cbb24368fc20f7b0b3ab06ecb341e9c
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-05-16 10:51:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// react
+import { lazy, Suspense } from "react";
+// react-router-dom
+import { Routes, Route } from "react-router-dom";
+
+// components
+import NavBar from "./components/NavBar/index.js";
+
+// pages
+const Home = lazy(() => import("./pages/Home/index.js"));
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container pt-2">
+        <Suspense fallback="Loading...">
+          <NavBar />
+
+          <Routes>
+            <Route index element={<Home />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+// react
+import React from "react";
+
+// styles
+import "./home.styles.scss";
+
+export default function Home() {
+  return (
+    <div className="home-page">
+      <div className="home-cont">
+        <div className="home-content">
+          home
+        </div>
+      </div>
+    </div>
+  );
+}
